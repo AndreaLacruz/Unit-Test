5f5ddc2b2424b2a932101d1e5ba777d35cc0897d
Andrea <andreacristinals9@gmail.com>
2020-05-14 06:50:50 -0300

    first commit

---
+++ unit-test/src/main/java/ar/com/ada/sb/unittest/model/entity/Product.java
@@ -0,0 +1,54 @@
+package ar.com.ada.sb.unittest.model.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigInteger;
+import java.util.Date;
+
+@Getter @NoArgsConstructor
+@Entity(name = "Product")
+public class Product {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, columnDefinition = "LONGTEXT")
+    private String description;
+
+    private BigInteger price;
+
+    @CreationTimestamp
+    private Date createAt;
+
+    @UpdateTimestamp
+    private Date updateAt;
+
+
+    public Product setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public Product setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public Product setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public Product setPrice(BigInteger price) {
+        this.price = price;
+        return this;
+    }
+
+}
